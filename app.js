35581d39ae820d238d6bf71e724572a0d2f6b3fd
NatalyBMota <interactivedesign@yahoo.com>
2026-02-03 20:09:58 -0500

    Followed along with instructor written code up until 9:23:00, but did not follow the instructions to deploy to Heroku, because it's no longer free.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config()
 require('express-async-errors')
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express')
 const app = express()
 
@@ -13,9 +20,25 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
+// if the application will be behind a proxy, as is the case in Heroku, we
+// need to enable the trust proxy setting with the line of code below
+app.set('trust proxy', 1)
+
+app.use(
+  rateLimiter({ 
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // limit each IP to 100 requests per windowMs
+  })
+)
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
+app.get('/', (req, res) => {
+  res.send('Jobs API')
+})
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
