b77f6ce0aed5a30c028bf476a97d39fc3a0d9c47
NatalyBMota <interactivedesign@yahoo.com>
2026-02-09 18:56:16 -0500

    Undid the previous changes to the addEdit.js button, because they made the login and the register buttons disappear from the application. Also, realized that there is a bug to be fixed with the logoff functionality, which I should address before proceeding.

--- public/addEdit.js
+++ public/addEdit.js
@@ -1,46 +1,32 @@
-addEditDiv.addEventListener("click", async (e) => {
-  if (inputEnabled && e.target.nodeName === "BUTTON") {
-    if (e.target === addingJob) {
-      enableInput(false);
+import { enableInput, inputEnabled, message, setDiv, token } from "./index.js";
+import { showJobs } from "./jobs.js";
 
-      let method = "POST";
-      let url = "/api/v1/jobs";
-      try {
-        const response = await fetch(url, {
-          method: method,
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({
-            company: company.value,
-            position: position.value,
-            status: status.value,
-          }),
-        });
+let addEditDiv = null;
+let company = null;
+let position = null;
+let status = null;
+let addingJob = null;
 
-        const data = await response.json();
-        if (response.status === 201) {
-          // 201 indicates a successful create
-          message.textContent = "The job entry was created.";
+export const handleAddEdit = () => {
+  addEditDiv = document.getElementById("edit-job");
+  company = document.getElementById("company");
+  position = document.getElementById("position");
+  status = document.getElementById("status");
+  addingJob = document.getElementById("adding-job");
+  const editCancel = document.getElementById("edit-cancel");
 
-          company.value = "";
-          position.value = "";
-          status.value = "pending";
-
-          showJobs();
-        } else {
-          message.textContent = data.msg;
-        }
-      } catch (err) {
-        console.log(err);
-        message.textContent = "A communication error occurred.";
+  addEditDiv.addEventListener("click", (e) => {
+    if (inputEnabled && e.target.nodeName === "BUTTON") {
+      if (e.target === addingJob) {
+        showJobs();
+      } else if (e.target === editCancel) {
+        showJobs();
       }
-
-      enableInput(true);
-    } else if (e.target === editCancel) {
-      message.textContent = "";
-      showJobs();
     }
-  }
-});
+  });
+};
+
+export const showAddEdit = (job) => {
+  message.textContent = "";
+  setDiv(addEditDiv);
+};
