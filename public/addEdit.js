17416126d059a22bd78bc238113c22639bfde094
NatalyBMota <interactivedesign@yahoo.com>
2026-02-09 19:26:08 -0500

    Had accidentally replaced the entire code in the addEdit.js file with the snipped of code including suggested changes to it, which appeared in the assignment's instructions after 'Next we need to make the changes so that we can create job entries. The addEdit.js module is changed as follows.' Undid these changes to recover the file's previous code, then compared the suggested changes to my code, to ensure that no new changes needed to be made to it.

--- public/addEdit.js
+++ public/addEdit.js
@@ -1,46 +1,75 @@
-addEditDiv.addEventListener("click", async (e) => {
-  if (inputEnabled && e.target.nodeName === "BUTTON") {
-    if (e.target === addingJob) {
-      enableInput(false);
+import {
+  inputEnabled,
+  setDiv,
+  message,
+  token,
+  enableInput,
+} from "./index.js";
+import { showJobs } from "./jobs.js";
 
-      let method = "POST";
-      let url = "/api/v1/jobs";
-      try {
-        const response = await fetch(url, {
-          method: method,
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({
-            company: company.value,
-            position: position.value,
-            status: status.value,
-          }),
-        });
+let addEditDiv = null;
+let company = null;
+let position = null;
+let status = null;
+let addingJob = null;
+let editCancel = null;
 
-        const data = await response.json();
-        if (response.status === 201) {
-          // 201 indicates a successful create
-          message.textContent = "The job entry was created.";
+export const handleAddEdit = () => {
+  addEditDiv = document.getElementById("edit-job");
+  company = document.getElementById("company");
+  position = document.getElementById("position");
+  status = document.getElementById("status");
+  addingJob = document.getElementById("adding-job");
+  editCancel = document.getElementById("edit-cancel");
 
-          company.value = "";
-          position.value = "";
-          status.value = "pending";
+  addEditDiv.addEventListener("click", async (e) => {
+    if (inputEnabled && e.target.nodeName === "BUTTON") {
+      if (e.target === addingJob) {
+        enableInput(false);
 
-          showJobs();
-        } else {
-          message.textContent = data.msg;
+        let method = "POST";
+        let url = "/api/v1/jobs";
+        try {
+          const response = await fetch(url, {
+            method: method,
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              company: company.value,
+              position: position.value,
+              status: status.value,
+            }),
+          });
+
+          const data = await response.json();
+          if (response.status === 201) {
+            // 201 indicates a successful create
+            message.textContent = "The job entry was created.";
+
+            company.value = "";
+            position.value = "";
+            status.value = "pending";
+
+            showJobs();
+          } else {
+            message.textContent = data.msg;
+          }
+        } catch (err) {
+          console.log(err);
+          message.textContent = "A communication error occurred.";
         }
-      } catch (err) {
-        console.log(err);
-        message.textContent = "A communication error occurred.";
-      }
 
-      enableInput(true);
-    } else if (e.target === editCancel) {
-      message.textContent = "";
-      showJobs();
+        enableInput(true);
+      } else if (e.target === editCancel) {
+        message.textContent = "";
+        showJobs();
+      }
     }
-  }
-});
+  });
+};
+
+export const showAddEdit = (jobID) => {
+  setDiv(addEditDiv);
+};
