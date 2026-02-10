61403b4aa5aba73bebf4821e122bae1b16d93ce8
NatalyBMota <interactivedesign@yahoo.com>
2026-02-09 19:42:57 -0500

    Changed the addEdit.js file, as instructed to do so by the assignment's instructions, right before where it says 'With this change, the add/edit div will be displayed with the appropriate values.'

--- public/addEdit.js
+++ public/addEdit.js
@@ -70,6 +70,49 @@ export const handleAddEdit = () => {
   });
 };
 
-export const showAddEdit = (jobID) => {
-  setDiv(addEditDiv);
+export const showAddEdit = async (jobId) => {
+  if (!jobId) {
+    company.value = "";
+    position.value = "";
+    status.value = "pending";
+    addingJob.textContent = "add";
+    message.textContent = "";
+
+    setDiv(addEditDiv);
+  } else {
+    enableInput(false);
+
+    try {
+      const response = await fetch(`/api/v1/jobs/${jobId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+      if (response.status === 200) {
+        company.value = data.job.company;
+        position.value = data.job.position;
+        status.value = data.job.status;
+        addingJob.textContent = "update";
+        message.textContent = "";
+        addEditDiv.dataset.id = jobId;
+
+        setDiv(addEditDiv);
+      } else {
+        // might happen if the list has been updated since last display
+        message.textContent = "The jobs entry was not found";
+        showJobs();
+      }
+    } catch (err) {
+      console.log(err);
+      message.textContent = "A communications error has occurred.";
+      showJobs();
+    }
+
+    enableInput(true);
+  }
+  // setDiv(addEditDiv);
 };
